3760d4a72615c10e56bb6a823ec42f1e7487952f
ElinaPark <elinapark0818@gmail.com>
2021-03-04 16:57:42 +0900

    자바스크립트 if문 기본

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -17,3 +17,11 @@ const f = '안녕'
 const g = '하니?'
 
 console.log(f+g);
+
+const aa = 1;
+if (aa + 1 === 2) {
+    console.log('aa + 1 이 2입니다.');
+    const aa = 2;
+    console.log('if문 안의 a 값은?' + aa);
+}
+console.log('if문 안의 a 값은?' + aa);
