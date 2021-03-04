08c12fa3d8af58d63e46570894e403636d843093
ElinaPark <elinapark0818@gmail.com>
2021-03-04 17:50:51 +0900

    객체구조분해 연습

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -140,7 +140,8 @@ console.log(ironMan);
 console.log(captainAmerica);
 
 function print(hero) {
-    const text = `${hero.alias}(${hero.name}) 역할을 맡은 배우는 ${hero.actor} 입니다.`;
+    const { alias, name, actor} = hero;
+    const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
     console.log(text);
 }
 print(ironMan);
