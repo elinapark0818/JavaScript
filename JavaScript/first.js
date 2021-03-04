f07b1530f1f8c59e978d18738ab495866f733a11
ElinaPark <elinapark0818@gmail.com>
2021-03-04 17:07:02 +0900

    switch 에서는 break를 활용해야 한다.

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -42,3 +42,20 @@ if (bb === 5) {
 } else {
     console.log('bb는 5도 10도 아니다.');
 }
+
+
+const device = 'galaxy note';
+
+switch (device) {
+    case 'iphone':
+        console.log('아이폰!')
+        break;
+    case 'ipad':
+        console.log('아이패드!')
+        break;
+    case 'galaxy note':
+        console.log('갤럭시 노트!')
+        break;
+    default:
+        console.log('모르겠다.')
+}
