9b4ec4afdb5047e9a801c1fa8660288b72156be2
ElinaPark <elinapark0818@gmail.com>
2021-03-05 00:02:00 +0900

    배열담을 공간과 푸쉬를 이용해서 풀었다.

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -201,35 +201,35 @@ console.log("Hello javaScript");
 // crazydog.name = '김동이';
 // console.log(crazydog._name);
 
-const numbers = {
-    _a: 1,
-    _b: 2,
-    sum: 3,
-    calculate() {
-        console.log('calculate');
-        this.sum = this._a + this._b;
-    },
-    get a() {
-        return this._a
-    },
-    get b() {
-        return this._b
-    },
-    set a(value) {
-        this._a = value;
-        this.calculate();
-    },
-    set b(value) {
-        this._b = value;
-        this.calculate();
-    }
-};
-
-console.log(numbers.sum);
-numbers.a = 5;
-numbers.b = 7;
-numbers.a = 9;
-console.log(numbers.sum);
+// const numbers = {
+//     _a: 1,
+//     _b: 2,
+//     sum: 3,
+//     calculate() {
+//         console.log('calculate');
+//         this.sum = this._a + this._b;
+//     },
+//     get a() {
+//         return this._a
+//     },
+//     get b() {
+//         return this._b
+//     },
+//     set a(value) {
+//         this._a = value;
+//         this.calculate();
+//     },
+//     set b(value) {
+//         this._b = value;
+//         this.calculate();
+//     }
+// };
+//
+// console.log(numbers.sum);
+// numbers.a = 5;
+// numbers.b = 7;
+// numbers.a = 9;
+// console.log(numbers.sum);
 
 
 //Setter 함수는 특정 값을 설정할 때마다
@@ -299,11 +299,16 @@ console.log(numbers.sum);
 // const result = sumOf([1,2,3,4,5]);
 // console.log(result);
 
-function biggerThanThree(nbs) {
-
+function biggerThanThree(numbers) {
+    const array = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 3) {
+            array.push(numbers[i]);
+        }
+    }
+    return array;
 }
 
-const nbs = [1,2,3,4,5,6,7];
-console.log(biggerThanThree(nbs));
-
-export default biggerThanThree;
+const numbers = [1,2,3,4,5,6,7];
+console.log(biggerThanThree(numbers));
+// export default biggerThanThree; 문제를 복사해왔지만, 안되길래 뺌..
