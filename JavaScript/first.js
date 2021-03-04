0c7619721bb6eec1eab75c56cf5068d2e817e448
ElinaPark <elinapark0818@gmail.com>
2021-03-04 16:53:43 +0900

    자바스크립트 복습

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -2,5 +2,18 @@ console.log("Hello javaScript");
 
 const a = null;
 const b = undefined;
-const equals = a === b;
+const equals = false;
 console.log(equals);
+
+const c = 20;
+const d = 25;
+const e = 20;
+
+console.log(c < d);
+console.log(e <= d);
+console.log(e > c);
+
+const f = '안녕'
+const g = '하니?'
+
+console.log(f+g);
