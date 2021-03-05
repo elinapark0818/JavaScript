81b7043c1dbc8c152a2522bd12fe4e870c3aeb1c
ElinaPark <elinapark0818@gmail.com>
2021-03-05 14:11:19 +0900

    findIndex

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -362,3 +362,34 @@ const items = [
 ];
 const texts = items.map(item => item.text);
 console.log(texts);
+
+
+// const superheroes = ['아이언맨', '캡틴 아메리카', '토르', '닥터 스트레인지'];
+// const index = superheroes.indexOf('토르');
+// console.log(index);
+
+const todos = [
+    {
+        id: 1,
+        text: '자바스크립트 입문',
+        done: true,
+    },
+    {
+        id: 2,
+        text: '함수 배우기',
+        done: true,
+    },
+    {
+        id: 3,
+        text: '객체와 배열 배우기',
+        done: true,
+    },
+    {
+        id: 4,
+        text: '배열 내장함수 배우기',
+        done: false
+    }
+]
+
+const index = todos.findIndex(todo => todo.id === 3)
+console.log(index);
