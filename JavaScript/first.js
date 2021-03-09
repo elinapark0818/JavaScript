453befcd9a5d90e1790727527a6e3c1dcc56c74d
ElinaPark <elinapark0818@gmail.com>
2021-03-10 00:11:22 +0900

    구구단 연습

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -665,3 +665,20 @@ console.log(!NaN);
 
 // Falsy : null, undefined, 0, '', NaN
 //     Truthy = Falsy가 아닌 것
+
+//N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오.
+// 출력 형식에 맞춰서 출력하면 된다.
+
+for(let i = 2; i <= 9; i++) {
+    console.log(i + '단');
+    for(let j = 1; j <= 9; j++) {
+        console.log(i + ' * ' + j + ' = ' + (i*j));
+    }
+}
+//
+
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const n = N(input[0]);
+for (let i= 1; i < 10; i++) {
+    console.log(`${n}*${i}=${n*i}`);
+}
