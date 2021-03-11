4c22aa4b10c833c27fbda0f012167085725f9a81
ElinaPark <elinapark0818@gmail.com>
2021-03-11 20:31:54 +0900

    비동기처리(setTimeout, callback)로 처리순서 설정하기

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -941,4 +941,23 @@ function myFunction() {
 
 myFunction();
 console.log('global scope: ');
-console.log(value);
+console.log(value)
+
+
+//비동기처리
+
+function work(callback) {
+    setTimeout(() => {
+        const start = Date.now();
+        for (let i = 0; i < 10000; i++) {}
+        const end = Date.now();
+        callback(end - start + 'ms');
+    }, 0);
+}
+
+console.log('작업 시작!');
+work((endTime) => {
+    console.log(endTime);
+    console.log('작업이 끝났어요');
+});
+console.log('다음 작업')
