f58102b7a2794f6dbb5a6d0b074cb0ec805a4daa
ElinaPark <elinapark0818@gmail.com>
2021-03-12 17:55:18 +0900

    카운터 (getElementById) (querySelectorAll)

--- JavaScript/counter.js
+++ JavaScript/counter.js
@@ -1,6 +1,22 @@
+// const number = document.getElementById("number");
+// const increase = document.getElementById("increase");  //ID가 있을 때(html)
+// const decrease = document.getElementById("decrease");
+//
+// increase.onclick = () => {
+//     const current = parseInt(number.innerText, 10);
+//     number.innerText = current + 1;
+// };
+//
+// decrease.onclick = () => {
+//     const current = parseInt(number.innerText, 10);
+//     number.innerText = current - 1;
+// };
+// parseInt 는 문자열을 숫자로 변환해주는 함수입니다. 두번째 10을 넣어준 것은, 10진수로 숫자를 받아오겠다는 의미
+
+
 const number = document.getElementById("number");
-const increase = document.getElementById("increase");
-const decrease = document.getElementById("decrease");
+const buttons = document.querySelectorAll("button"); //id가 없을 때(html)
+const [increase, decrease] = buttons;
 
 increase.onclick = () => {
     const current = parseInt(number.innerText, 10);
