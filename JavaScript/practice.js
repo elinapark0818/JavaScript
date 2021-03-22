183efb144a9e84a7c5fc7315dddfeb864a98702f
ElinaPark <elinapark0818@gmail.com>
2021-03-22 13:55:44 +0900

    객체(object)

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -86,3 +86,19 @@ function hello(callback) {
 hello(function () {
     console.log('콜백');
 });
+
+//생성자 함수
+function A() {}
+
+const a = new A();
+console.log(a, typeof a);
+console.log(A());
+
+// 생성하면서 데이터 넣기
+function B(name, age) {
+    console.log(name,age);
+}
+
+const b = new B();
+const c = new B('Elina', 30);
+console.log(B());
