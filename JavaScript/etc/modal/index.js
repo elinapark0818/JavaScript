2842a4827b7c50f1a3f9a9efe493eaa0e3e31d72
ElinaPark <elinapark0818@gmail.com>
2021-03-25 21:58:41 +0900

    패키지 제이슨 npm 설치 스타트 모달 출력

--- JavaScript/etc/modal/index.js
+++ JavaScript/etc/modal/index.js
@@ -1,13 +1,12 @@
-import './style.css';
-console.log("fff")
+import "./style.css";
+
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const modal = document.querySelector(".modal-wrapper");
-
 open.onclick = () => {
+    console.log("ffasd");
     modal.style.display = "flex";
-};  //왜 안되는거니...왜..왜..정말 그럴거니?...플렉스라니까?...보여져야지..
-
+};
 close.onclick = () => {
     modal.style.display = "none";
 };
