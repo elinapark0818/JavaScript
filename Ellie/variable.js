dfd8334df4478ed68b92a439c54251a89847de88
ElinaPark <elinapark0818@gmail.com>
2021-03-26 16:05:55 +0900

    variable 복습노트 작성

--- Ellie/variable.js
+++ Ellie/variable.js
@@ -13,7 +13,9 @@ console.log(globalName);
 // Contant
 
 const daysInWeek = 7;
+console.log(daysInWeek);
 const maxNumber = 5;
+console.log(maxNumber);
 
 // Variable types
 // primitive, single item: number, string, boolean, null, undefined, symbol
@@ -43,4 +45,50 @@ const char = 'c';
 const brendan = 'brendan';
 const greeting = 'hello' + brendan;
 console.log(`value: ${greeting}, type: ${typeof greeting}`);
-const helloBob = ``
+const helloBob = `hi ${brendan}!`; //template leterals(string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
+
+//boolean
+//false: 0, null, undefined, NaN, ''
+//true: any other value
+const canRead = true;
+const test = 3 < 1; //false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+//null 빈 값으로 선언된거야
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+//undefined 선언은 되었지만 정해진게 없어
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+//symbol, create unique identifiers for objects 우선수위를 주고 싶을 때 고유한 식별자로
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);  //false 동일하지 않아
+
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1.description}`);
+//.description을 이용해서 string으로 변환해서 출력해야한다.
+
+//object, real-life object, data structure
+const elina = { name: 'elina', age: 30};
+elina.age = 23;
+
+// Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); // h
+console.log(`value: ${text}, type: ${typeof text}`); // hello, string
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`); // 1, number
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`); // 75,string
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`); // 4, number
+
+// console.log(text.charAt(0)); // TypeError
