bc5d28c6aebfec2abb9f5fb0f38545131682e4ba
ElinaPark <elinapark0818@gmail.com>
2021-03-27 23:41:04 +0900

    class

--- Ellie/class.js
+++ Ellie/class.js
@@ -16,4 +16,23 @@
 
 // JavaScript classes
 // - introduced in ES6
-// - syntactical sugar over prototyped-based inheritance
+// - syntactical sugar over prototyped-based inheritance 프로토타입을 기반으로 문법만 생긴거다
+
+// Class declarations
+class Person {
+    //constructor 생성자
+    constructor(name, age) {
+        //fields
+        this.name = name;
+        this.age = age;
+    }
+    // methods
+    speak() {
+        console.log(`${this.name}: hello!`);
+    }
+}
+
+const elina = new Person('elina', 30);
+console.log(elina.name);
+console.log(elina.age);
+elina.speak();
