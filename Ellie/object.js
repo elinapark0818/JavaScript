a1ffa9b47c1959d171abf91589ecae74e5f8d2d7
ElinaPark <elinapark0818@gmail.com>
2021-03-28 21:51:04 +0900

    object, Literals and properties

--- Ellie/object.js
+++ Ellie/object.js
@@ -4,6 +4,7 @@
 // one of the JavaScript's data types
 // a collection of related data and/or fuctionality
 // Nearly all objects in JavaScript are instances of Object
+// Object = {key : value}; 오브젝트는 키와 밸류의 집합체다
 
 // const name = 'elina';
 // const age = 5;
@@ -13,18 +14,26 @@
 //     console.log(age);
 // }
 
-const obj1 = {};
-const obj2 = new Object();
+// Literals and properties
+const obj1 = {}; // 'object literal' syntax
+const obj2 = new Object(); // 'object constructor' syntax
 
 function print(person) {
     console.log(person.name);
     console.log(person.age);
 }
 
-const hoo = {name: 'hoo', age: 29};
+const hoo = {name: 'hoo', age: 29}; //자바스크립트는 다이나믹 런타임 가능
 print(hoo);
 
+// with Javascript magic (dynamically typed language)
+// can add properties later
+hoo.hasJob = true;
+console.log(hoo.hasJob); // 유지보수가 어렵당
 
+// can delete properties later
+delete hoo.hasJob;
+console.log(hoo.hasJob);
 
 
 
