c855f4e78ef29b8e4834539d0839925adb530ff5
ElinaPark <elinapark0818@gmail.com>
2021-03-28 21:56:36 +0900

    object, Computed properties

--- Ellie/object.js
+++ Ellie/object.js
@@ -35,8 +35,18 @@ console.log(hoo.hasJob); // 유지보수가 어렵당
 delete hoo.hasJob;
 console.log(hoo.hasJob);
 
+// Computed properties 계산된 프로퍼티즈
+// key should be always string
+console.log(hoo.name); //코딩하는 순간 받아올때 쓴다
+console.log(hoo['name']); //string 가져오기 런타임에서 결정될 때 쓴다(뭔지 정확히 모를때..?)
+hoo['hasJob'] = true;
+console.log(hoo.hasJob);
 
-
+function printValue(obj, key) {
+    console.log(obj[key]);
+}
+printValue(hoo, 'name');
+printValue(hoo, 'age'); //동적으로 key에 관련된 값을 불러올때 유용하게 사용될 수 있다
 
 
 
