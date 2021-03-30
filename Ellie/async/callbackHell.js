7b4c9d339325e10f3bb7d0f0daa826f9160e4bea
ElinaPark <elinapark0818@gmail.com>
2021-03-30 20:33:05 +0900

    callbackHell 직접 연습해보기

---
+++ Ellie/async/callbackHell.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class DataStorage {
+    loginData (id, password, onSuccess, onError) {
+        setTimeout(()=> {
+            if (
+                (id === 'elina' && password === '1234') ||
+                (id === 'hoomin' && password === '1234')) {
+                    onSuccess(id);
+            } else {
+                    onError(new Error('Id is not found'));
+            }
+        }, 2000);
+    }
+    adminData (id, onSuccess, onError) {
+        setTimeout(()=> {
+            if (id === 'elina') {
+                onSuccess({id:'elina', admin:'admin'})
+            } else {
+                onError (new Error('Admin is not access'));
+            }
+        }, 1000);
+    }
+}
+
+const dataStorage = new DataStorage();
+const id = prompt('send your ID');
+const password = prompt('send your PW');
+dataStorage.loginData(
+    id,
+    password,
+    id => {
+        dataStorage.adminData(
+            id,
+            idWithAdmin => {
+                alert(`Hello ${idWithAdmin.id}, you have a ${idWithAdmin.admin} ACCESS`);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    },
+    error => {
+        console.log(error);
+    }
+)
