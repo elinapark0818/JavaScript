84b2d98ed0c1892b3b6fd9de9c9b99693554fa58
ElinaPark <elinapark0818@gmail.com>
2021-03-30 18:30:09 +0900

    callback synchronous

---
+++ Ellie/async/callback.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// JavaScript is synchronous 자바스크립트는 동기적이다
+// Excute the code block by orger after hoisting.
+// hoisting : var, function declaration 과 같은 선언들이 제일 위로 올라가는 것
+
+console.log('1');
+setTimeout(function () {
+    console.log('2');
+}, 1000); // 브라우저 API, 브라우저에게 먼저 요청한다
+console.log('3');
