e3e519dec1e9294953e1d841d22fc272e3083635
ElinaPark <elinapark0818@gmail.com>
2021-03-30 16:49:11 +0900

    array 퀴즈 풀이

--- Ellie/array-api.js
+++ Ellie/array-api.js
@@ -38,7 +38,6 @@ const array12 = [1, 2, 3, 4, 5];
 // const result14 = array12.splice(0, 2); //배열자체를 수정
 // console.log(result14);
 // console.log(array12);
-
 const result14 = array12.slice(2, 5); //배열에서 원하는 부분만 리턴해서 받아오고 싶을 때
 console.log(result14); // [3, 4, 5]
 console.log(array12); // [1, 2, 3, 4, 5]
@@ -65,7 +64,6 @@ const students = [
 // Q5. fine a student with the score 90
 const highScore = students.filter(students => students.score > 89);
 console.log(highScore);
-
 // ellie
 const result15 = students.find((student) => student.score === 90);
 console.log(result15);
@@ -74,22 +72,69 @@ console.log(result15);
 // Q6. make an array of enrolled students
 const enrolled = students.filter(students => students.enrolled === true);
 console.log(enrolled);
+// ellie 드디어 같다ㅠㅠ
+const result16 = students.filter((student) => student.enrolled);
+console.log(result16);
+
 
 // Q7. make an array containing only the student's scores
 // result should be : [45, 80, 90, 66, 88]
 
-const enrolledMap = students.map(function (enrolled) {
-    return enrolled;
+const scoreMap = students.map(function (student) {
+    return student.score;
 });
-console.log(enrolledMap);
+console.log(scoreMap);
+//ellie
+const result17 = students.map((student) => student.score); // 콜백함수 인자 이름은 이해하기 쉽게 짓자
+console.log(result17);
+
 
 // Q8. check if there is a student with the score lower than 50
+const lowScore = students.some((student) => student.score < 50);
+console.log(lowScore);
+//ellie
+const result18 = students.some((student) => student.score < 50);
+console.log(result18);
 
+const result = !students.every((student) => student.score < 50);
+console.log(result); // 모든 점수가 50 보다 작다면 true 나오고, 아니면 false 앞에 ! 붙이면 반대
+
+console.clear()
 // Q9. compute student's average score
+
+const result19 = students.reduce((previousValue, currentValue) => {
+    console.log('-----------');
+    console.log(previousValue);
+    console.log(currentValue);
+    return previousValue + currentValue.score;
+}, 0);
+console.log(result19);
+//
+//const result19 = students.reduce((previousValue, currentValue) => previousValue + currentValue.score, 0);
+//console.log(result19);
+
+const result20 = students.reduce((previousValue, currentValue) => previousValue + currentValue.score, 0);
+console.log(result20 / students.length);
 
 // Q10. make a string containing all the scores
 // result should be : '45, 80, 90, 66, 88'
-
+const result21 = students
+    .map(student => student.score) //점수로 변환해서
+    .filter(score => score >= 50) // 조건 필터링해서
+    .join(); // 조인한다
+console.log(result21);
 
 // Bonus! do Q10 sorted in ascending order
 // result should be : '45, 66, 80, 88, 90'
+
+//오름차순
+const result22 = students.map(student => student.score)
+    .sort((a, b) => a - b)
+    .join();
+console.log(result22);
+
+//내림차순
+const result23 = students.map(student => student.score)
+    .sort((a, b) => b - a)
+    .join();
+console.log(result23);
