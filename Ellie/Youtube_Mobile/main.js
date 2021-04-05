f2c1c3c0d1b61f1f6de19822bd541c5f8f1779ed
ElinaPark <elinapark0818@gmail.com>
2021-04-06 01:19:45 +0900

    클론 코딩(Youtube) 내일 다시 고치자

---
+++ Ellie/Youtube_Mobile/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
+const title = document.querySelector('.info .metadata .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
